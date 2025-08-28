e2abb4419892cfc565be6a60fb767a56180fea37
ad-dw <aditya_dwivedi01@outlook.com>
2025-08-28 13:38:08 +0530

    fixing debouncing logic.

--- src/helpers/debounce.js
+++ src/helpers/debounce.js
@@ -1,10 +1,8 @@
 export function debounce(callback, delay = 1000) {
-  console.log("debounce", this);
   let timer; // Variable to store the timeout ID
 
-  return function (args) {
-    console.log(this);
+  return function (event) {
     clearTimeout(timer); // Clear the previous timeout
-    timer = setTimeout(callback(args), delay); // Set a new timeout
+    timer = setTimeout(() => callback(event), delay); // Set a new timeout
   };
 }

a5a08c904c9a12763abbb47e0be726dc71fbbb74
ad-dw <aditya_dwivedi01@outlook.com>
2025-10-17 11:46:52 +0530

    Adding polyfills.

---
+++ src/polyfills.js
@@ -0,0 +1,6 @@
+// stable language features (Array.from, Promise, etc.)
+import "core-js/stable";
+// async/await runtime
+import "regenerator-runtime/runtime";
+// fetch polyfill for older browsers
+import "whatwg-fetch";
